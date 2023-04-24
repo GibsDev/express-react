e5d7f4d36247c9b9d63edfccc354331707a3e5e9
GibsDev <notcurtisg@gmail.com>
2023-04-23 21:58:00 -0400

    Basic sockets

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,33 @@
+import {io} from 'socket.io-client';
+import {useEffect, useState} from 'react';
+
 const App = () => {
-    return <p>App here</p>;
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const socket = io();
+
+            socket.on("connect", () => {
+                console.log('Connected to socket');
+            });
+
+            setSocket(socket);
+        })();
+    }, []);
+
+    const fire = () => {
+        if (!socket) return;
+        socket.emit('msg', 'Hello There!')
+    };
+
+    return <div>
+        <p>App here</p>
+        {socket && <>
+            <p>Connected</p>
+            <button onClick={fire}>Message!</button>
+        </>}
+    </div>;
 }
 
 export default App;
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,23 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const { createServer } = require('http');
+import { Server } from 'socket.io';
 const port = 3000;
-const path = require('path');
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {});
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(port, () => {
+io.on("connection", socket => {
+    socket.on('msg', payload => {
+        console.log(payload);
+    });
+});
+
+httpServer.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
     console.log(`http://localhost:${port}`);
 });
